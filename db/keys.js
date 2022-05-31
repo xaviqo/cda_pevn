f39b0f7001a8c54956dd1877ae6efe3fb6e26449
xaviqo <silent90s@gmail.com>
2022-05-31 10:17:23 +0200

    Minor changes

---
+++ db/keys.js
@@ -0,0 +1,9 @@
+import {Pool} from 'pg';
+
+const pool = new Pool({
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    password: '1337',
+    database: 'cdadb'
+});
---
+++ routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.send('Registred');
+})
