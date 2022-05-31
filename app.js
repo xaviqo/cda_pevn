74cb2d8c380e18073644b7dbc367f9304448e6ae
xaviqo <silent90s@gmail.com>
2022-05-31 07:30:57 +0200

    WIP

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import history from "connect-history-api-fallback";
+import path from "path";
+
+const app = express();
+
+
+// MIDDLEWARES
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('hey!');
+});
+
+// SETTINGS
+app.set('port', process.env.PORT || 3000);
+app.listen(3000, () => {
+    console.log("Server @ port " + app.get('port'));
+});
