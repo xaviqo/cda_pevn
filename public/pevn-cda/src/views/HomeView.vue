36b97f05e6b4d9cccd14d1c06af7f901ebccabc6
xaviqo <silent90s@gmail.com>
2022-06-02 20:31:26 +0200

    Working on VUE

---
+++ public/pevn-cda/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title>
+      <strong>
+        CARROUSEL DE ACTORES
+      </strong>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
