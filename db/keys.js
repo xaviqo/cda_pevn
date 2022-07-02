e127d4f2990380882f0d12bd3dd189129838b93a
xaviqo <silent90s@gmail.com>
2022-07-02 11:55:24 +0200

    Finished with videos and social networks

--- db/keys.js
+++ db/keys.js
@@ -1,11 +1,11 @@
 import {Pool} from 'pg';
 
 const pool = new Pool({
-    host: 'ec2-3-248-121-12.eu-west-1.compute.amazonaws.com',
+    host: 'localhost',
     port: '5432',
-    user: 'jijvjylxqufosp',
-    password: '69990da223389dc1c694a5f35777e56901f8172ea8225433208eb264536717d0',
-    database: 'd66crckd29vk0'
+    user: 'postgres',
+    password: process.env.DB_PASS_PG,
+    database: 'cdadb'
 });
 
 module.exports = pool;
